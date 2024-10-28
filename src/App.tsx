35ab7bc0aa06566be6a5ab98e24a79dca264581e
ELODY FORT <elody.fort.professionnel@gmail.com>
2024-10-28 12:23:38 +0100

    pokemonsuivent

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
+import React, { useState } from "react";
 
 interface Pokemon {
   name: string;
@@ -13,16 +14,50 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
 function App() {
-  const pokemon = pokemonList[0];
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  const pokemon = pokemonList[pokemonIndex];
+
+  const handlePrevious = () => {
+    setPokemonIndex(pokemonIndex - 1);
+  };
+
+  const handleNext = () => {
+    setPokemonIndex(pokemonIndex + 1);
+  };
   console.log(pokemon);
+
   return (
     <>
       <PokemonCard pokemon={pokemon} />
+      <button onClick={handlePrevious} disabled={pokemonIndex === 0}>
+        Précédent
+      </button>
+      <button
+        onClick={handleNext}
+        disabled={pokemonIndex === pokemonList.length - 1}
+      >
+        Suivant
+      </button>
     </>
   );
 }
